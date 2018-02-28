1720a2c55425ab4b55ab19e33602fd00aa3a79e6
Tenth Doctor <doctorandabox@gmail.com>
2018-02-28 22:03:04 +0530

    basics

--- Basics/pyfund/words.py
+++ Basics/pyfund/words.py
@@ -1,7 +1,9 @@
+import sys
 from urllib.request import urlopen
 
-def fetch_words():
-    with urlopen('http://sixty-north.com/c/t.txt') as story:
+
+def fetch_words(url):
+    with urlopen(url) as story:
         story_words = []
         for line in story:
             line_words = line.decode('utf-8').split()
@@ -9,13 +11,16 @@ def fetch_words():
                 story_words.append(word)
     return story_words
 
-def print_words(story_words):
+
+def print_items(story_words):
     for word in story_words:
         print(word)
 
-def main():
-    words = fetch_words()
-    print_words(words)
+
+def main(url):
+    words = fetch_words(url)
+    print_items(words)
+
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
